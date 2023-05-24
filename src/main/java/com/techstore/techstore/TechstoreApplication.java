4581375a70df489dffd57a22f9418cea56964951
vanminh <minhp4541@gmail.com>
2023-05-24 16:10:28 +0700

    first commit

---
+++ src/main/java/com/techstore/techstore/TechstoreApplication.java
@@ -0,0 +1,13 @@
+package com.techstore.techstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechstoreApplication.class, args);
+	}
+
+}
